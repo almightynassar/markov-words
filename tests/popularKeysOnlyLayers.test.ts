2f38bd8d2a4761f03be7f3f117c91cff5b31dab8
Nassar Zeitoune <nassar.z@slair.com.au>
2023-02-22 00:48:26 +1100

    Added test with Object keys

---
+++ tests/popularKeysOnlyLayers.test.ts
@@ -0,0 +1,21 @@
+import MarkovWords from '../src'
+import popular1900boys from './data/popular1900boys'
+
+const markov = new MarkovWords(Object.keys(popular1900boys), 3)
+
+describe('Filled Constructor', () => {
+  it('should have sum', () => {
+    expect(markov.data.meta.sum).toBeGreaterThan(0)
+  })
+  it('should have total', () => {
+    expect(markov.data.meta.total).toBeGreaterThan(0)
+  })
+  it('should generate word', () => {
+    const name = markov.generate()
+    expect(name).toBeDefined()
+  })
+  it('should generate list of words', () => {
+    const list = markov.generateList(100)
+    expect(list).toHaveLength(100)
+  })
+})
